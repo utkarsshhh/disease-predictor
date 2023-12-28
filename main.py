ddd1b12a51bdeb44c1619ff76a9bcf5ecb463a4a
Utkarsh Padia <utkarshpadia@gmail.com>
2023-12-28 22:57:09 +0530

    data processing

--- main.py
+++ main.py
@@ -8,13 +8,30 @@ data = pd.read_csv('heart+disease/processed.cleveland.data',delimiter=',',names=
 
 print (data.head())
 
-print (data.dtypes)
+# print (data.dtypes)
 
-print (data[['ca','thal']])
+# print (data[['ca','thal']])
 
 # data[['ca','thal']] = data[['ca','thal']].apply(pd.to_numeric)
-data['ca'] = pd.to_numeric(data.ca,errors='coerce')
-data['thal'] = pd.to_numeric(data.thal,errors='coerce')
+# data['ca'] = pd.to_numeric(data.ca,errors='coerce')
+# data['thal'] = pd.to_numeric(data.thal,errors='coerce')
 
+cat_features = []
 
-print (data.dtypes)
+for i in data.columns:
+    if (len(data[i].value_counts())<5):
+        print (i," : ",data[i].value_counts())
+        cat_features.append(i)
+# print (cat_features)
+
+data = data.drop(data[data['thal']=='?'].index)
+
+df = pd.get_dummies(data,columns=cat_features,prefix = cat_features)
+#
+print (df.head())
+
+# sex, cp, fbs, restecg, exang, slope, ca, thal,
+
+print (data.isnull().sum())
+print (data['num'])
+# print (data.dtypes)
