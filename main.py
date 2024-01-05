971bd21f7494c8a1a531f757c5f461be388c3373
Utkarsh Padia <utkarshpadia@gmail.com>
2024-01-05 22:49:07 +0530

    cleaning

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import torch
 import torch.nn as nn
-
+from sklearn.preprocessing import OneHotEncoder
+from torch.optim import SGD
 columns = ['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal','num']
 
 data = pd.read_csv('heart+disease/processed.cleveland.data',delimiter=',',names=columns)
@@ -12,15 +13,14 @@ print (data.head())
 
 # print (data[['ca','thal']])
 
-# data[['ca','thal']] = data[['ca','thal']].apply(pd.to_numeric)
-# data['ca'] = pd.to_numeric(data.ca,errors='coerce')
-# data['thal'] = pd.to_numeric(data.thal,errors='coerce')
+data['ca'] = pd.to_numeric(data.ca,errors='coerce')
+data['thal'] = pd.to_numeric(data.thal,errors='coerce')
 
 cat_features = []
 
 for i in data.columns:
     if (len(data[i].value_counts())<5):
-        print (i," : ",data[i].value_counts())
+        # print (i," : ",data[i].value_counts())
         cat_features.append(i)
 # print (cat_features)
 
@@ -28,10 +28,40 @@ data = data.drop(data[data['thal']=='?'].index)
 
 df = pd.get_dummies(data,columns=cat_features,prefix = cat_features)
 #
-print (df.head())
+# print (df['num'])
 
 # sex, cp, fbs, restecg, exang, slope, ca, thal,
 
-print (data.isnull().sum())
-print (data['num'])
-# print (data.dtypes)
+print (df.isnull().sum())
+print (df['num'].value_counts())
+print (df.dtypes)
+
+print (df.shape)
+
+df.dropna(axis=0,inplace=True)
+
+
+features = df.drop(labels='num',axis =1)
+target = df[['num']]
+
+encoder = OneHotEncoder(sparse=False)
+
+encoded_target=encoder.fit_transform(target)
+encoded_target = torch.tensor(encoded_target,dtype=torch.float32)
+
+features = torch.tensor(features.values,dtype=torch.float32)
+
+class Model(nn.Module):
+    def __init__(self,input_size,output_size):
+        super(Model,self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_size,64),
+            nn.ReLU(),
+            nn.Linear(64,32),
+            nn.ReLU(),
+            nn.Linear(32,output_size)
+        )
+
+    def forward(self,input):
+        return self.model(input)
+
